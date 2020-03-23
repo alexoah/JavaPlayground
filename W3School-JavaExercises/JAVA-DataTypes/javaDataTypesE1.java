e81a8980b5f4d0fc1fbd78ed820e699299a1dc21
Alexoah YT <alexandra.soah@gmail.com>
2020-03-23 14:24:33 +0700

    added answer files for JAVA Data Types exercises

---
+++ W3School-JavaExercises/JAVA-DataTypes/javaDataTypesE1.java
@@ -0,0 +1,18 @@
+/**
+ *	from JAVA Data Types: Exercise 1 ( https://www.w3schools.com/java/exercise.asp?filename=exercise_data_types1 )
+ *
+ *	question:
+ *	Add the correct data type for the following variables:
+ *	
+ *	___ myNum = 9;
+ *	_____ myFloatNum = 8.99f;
+ *	____ myLetter = 'A';
+ *	_______ myBool = false;
+ *	______ myText = "Hello World"; 
+ *
+ */
+int myNum = 9;
+float myFloatNum = 8.99f;
+char myLetter = 'A';
+boolean myBool = false;
+String myText = "Hello World";
---
+++ W3School-JavaExercises/JAVA-DataTypes/javaDataTypesE3.java
@@ -0,0 +1,12 @@
+/**
+ *	from JAVA Data Types: Exercise 3 ( https://www.w3schools.com/java/exercise.asp?filename=exercise_data_types3 )
+ *
+ *	question:
+ *	Type casting - convert the following double type (myDouble) to an int type:
+ *	
+ *	double myDouble = 9.78;
+ *	int myInt = _____ myDouble;
+ *
+ */
+double myDouble = 9.78;
+int myInt = (int) myDouble;
