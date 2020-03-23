ccc24430b5e6a224d416a1273e9fbf48cd82ba49
Alexoah YT <alexandra.soah@gmail.com>
2020-03-23 14:14:33 +0700

    added answer files for JAVA Syntax exercises

---
+++ W3School-JavaExercises/JAVA-Syntax/javaSyntaxE1.java
@@ -0,0 +1,18 @@
+/**
+ *	from JAVA Syntax: Exercise 1 ( https://www.w3schools.com/java/exercise.asp?filename=exercise_syntax1 )
+ *
+ *	question:
+ *	Insert the missing part of the code below to output "Hello World".
+ *
+ *	public class MyClass {
+ *		public static void main(String[] args) {
+ *			______.___._______("Hello World");
+ *		}
+ *	}
+ *
+ */
+public class MyClass {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ W3School-JavaExercises/JAVA-Syntax/javaSyntaxE2.java
@@ -0,0 +1,12 @@
+/**
+ *	from JAVA Syntax: Exercise 2 ( https://www.w3schools.com/java/exercise.asp?filename=exercise_syntax2 )
+ *
+ *	question:
+ *	Comments in Java are written with special characters. Insert the missing parts:
+ *	
+ *	__ This is a single-line comment
+ *	__ This is a multi-line comment __
+ *
+ */
+//	This is a single-line comment
+/*	This is a multi-line comment */
