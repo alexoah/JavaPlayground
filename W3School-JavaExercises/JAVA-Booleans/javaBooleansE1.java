1a89abe46228eb15920e72d57d0e7db3b843f4d1
Alexoah YT <alexandra.soah@gmail.com>
2020-03-23 17:03:48 +0700

    added answer files for JAVA Booleans exercises

---
+++ W3School-JavaExercises/JAVA-Booleans/javaBooleansE1.java
@@ -0,0 +1,16 @@
+/**
+ *	from JAVA Booleans: Exercise 1 ( https://www.w3schools.com/java/exercise.asp?filename=exercise_booleans1 )
+ *
+ *	question:
+ *	Fill in the missing parts to print the values true and false:
+ *	
+ *	_______ isJavaFun = true;
+ *	_______ isFishTasty = false;
+ *	System.out.println(isJavaFun);
+ *	System.out.println(isFishTasty);
+ *
+ */
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);
+System.out.println(isFishTasty);
---
+++ W3School-JavaExercises/JAVA-Booleans/javaBooleansE2.java
@@ -0,0 +1,14 @@
+/**
+ *	from JAVA Booleans: Exercise 2 ( https://www.w3schools.com/java/exercise.asp?filename=exercise_booleans2 )
+ *
+ *	question:
+ *	Fill in the missing parts to print the value true:
+ *	
+ *	int x = 10;
+ *	int y = 9;
+ *	System.out.println(_ _ _);
+ *
+ */
+int x = 10;
+int y = 9;
+System.out.println(x > y);
