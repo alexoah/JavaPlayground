74ec367cf3d140a250ecf9ef5adae80b9e0e1bad
Alexoah YT <alexandra.soah@gmail.com>
2020-03-24 15:50:46 +0700

    updated exercise 3 file.

--- W3School-JavaExercises/JAVA-Classes_Objects/javaClasses_ObjectsE3.java
+++ W3School-JavaExercises/JAVA-Classes_Objects/javaClasses_ObjectsE3.java
@@ -4,14 +4,14 @@
  *	question:
  *	Use myObj to access and print the value of the x attribute of MyClass.
  *
-public class MyClass {
-	int x = 5;
-
-	public static void main(String[] args) {
-		MyClass myObj = new MyClass();
-		System.out.println(_____._);
-	}
-} 
+ *	public class MyClass {
+ *		int x = 5;
+ *
+ *		public static void main(String[] args) {
+ *			MyClass myObj = new MyClass();
+ *			System.out.println(_____._);
+ *		}
+ *	} 
  *
  */
 public class MyClass {
